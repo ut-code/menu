80e877c31f0042098f9a553dd88c83916e375228
bvv-1 <fhinnoigsr@gmail.com>
2023-02-06 12:01:35 +0900

    componentsとpagesフォルダを追加した

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Footer() {
+  return (
+    <footer>
+      <hr></hr>
+      <p>© Copyright {new Date().getFullYear()} ut.code();</p>
+    </footer>
+  )
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { useState } from "react"
+
+import Header from "./../components/Header"
+import Footer from "./../components/Footer"
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
