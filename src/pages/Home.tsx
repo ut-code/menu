85a0620bf90fee23f9f22abcdc71eb32306fd404
bvv-1 <fhinnoigsr@gmail.com>
2023-02-06 12:09:21 +0900

    cssファイルを置く場所を@/assets/cssに変更

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 
 import Header from "./../components/Header"
 import Footer from "./../components/Footer"
+import "@/assets/css/style.css"
 
 export default function Home() {
   return (
