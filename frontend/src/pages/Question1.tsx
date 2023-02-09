44200341f1522dfc020ade9c37c2991bc778b4df
bvv-1 <fhinnoigsr@gmail.com>
2023-02-09 15:43:57 +0900

    question1の枠だけ

--- frontend/src/pages/Question1.tsx
+++ frontend/src/pages/Question1.tsx
@@ -1,20 +1,22 @@
-import { useState, useEffect } from "react"
-
-import Header from "@/components/Header"
-import Footer from "@/components/Footer"
 import "@/assets/css/style.css"
 
 export default function Home() {
   return (
     <>
-      <Header />
-      <ul>
-        {/* {recipes.map((recipe) => (
-          <li key={recipe.categoryId}>{recipe.categoryName}</li>
-        ))} */}
-      </ul>
+      <div className="style1">
+        <div className="howToPlay">?</div>
+        <h1>使いたい食材はなんですか？</h1>
+        <p>食材の名前を入力してみましょう</p>
+        <div className="inputIngredient">a</div>
+        <div className="suggestIngredient">
+          <div className="box">季節の野菜1</div>
+          <div className="box">季節の野菜2</div>
+          <div className="box">季節の野菜3</div>
+          <div className="box">季節の野菜4</div>
+        </div>
+        <div className="nextButton">Next</div>
+      </div>
       <a href="/message">掲示板</a>
-      <Footer />
     </>
   )
 }
