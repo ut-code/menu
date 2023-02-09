a6c5ae82ee08781f7595db01227e8a327921e97b
bvv-1 <fhinnoigsr@gmail.com>
2023-02-09 16:34:30 +0900

    Question1.tsxを用いてとりあえず実装

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom"
 import Home from "@/pages/Home"
 import Message from "@/pages/Message"
 import Question1 from "@/pages/Question1"
+import Question2 from "@/pages/Question2"
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/home" element={<Home />}></Route>
         <Route path="/message" element={<Message />}></Route>
         <Route path="/question-1" element={<Question1 />}></Route>
+        <Route path="/question-2" element={<Question2 />}></Route>
       </Routes>
     </>
   )
