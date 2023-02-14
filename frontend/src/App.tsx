c84ac24658785573a5a13cbe854e7e7a67fbd0b2
Yokomi422 <rayzou1120@icloud.com>
2023-02-14 10:49:57 +0900

    make-result

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom"
 import Home from "@/pages/Home"
 import Message from "@/pages/Message"
 import Questions from "@/pages/Questions"
-
+import Result from "@/pages/Result"
 export default function App() {
   return (
     <>
@@ -11,6 +11,7 @@ export default function App() {
         <Route path="/home" element={<Home />}></Route>
         <Route path="/message" element={<Message />}></Route>
         <Route path="/questions" element={<Questions />}></Route>
+        <Route path="/result" element={<Result />}></Route>
       </Routes>
     </>
   )
