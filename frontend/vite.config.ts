351f17d694095112949f870d542b9d4318a409d5
bvv-1 <fhinnoigsr@gmail.com>
2023-03-01 03:03:45 +0900

    /manifestを/publicに変更することでbuildで取り込んでiconを読み込むように直し、PWA可能に

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -41,7 +41,6 @@ export default defineConfig({
             src: "./icon-512x512.png",
             sizes: "512x512",
             type: "image/png",
-            purpose: "any maskable",
           },
         ],
       },
