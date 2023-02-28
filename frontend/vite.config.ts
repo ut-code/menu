c61c491c9faa34bfa13383b11723a2eae5698ff4
bvv-1 <fhinnoigsr@gmail.com>
2023-02-28 13:39:57 +0900

    viteのバージョンをあげた、

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
 import react from "@vitejs/plugin-react"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    // use vitepwa to generate manifest.json and service-worker.js
+    VitePWA({
+      registerType: "autoUpdate",
+    }),
+  ],
 })
