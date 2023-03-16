ddf6a3b2460267b7440f332a8f02e10f6ef937f5
bvv-1 <fhinnoigsr@gmail.com>
2023-03-17 01:07:55 +0900

    RecipeCard.tsxを作成
    ・Result.tsxのカード部分をコンポーネント化
    ・card.cssからCSS modulesを使ったRecipeCard.module.cssに移行

---
+++ frontend/src/components/RecipeCard.tsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom"
+
+import TipReference from "@/components/TipReference"
+import styles from "@/assets/css/RecipeCard.module.css"
+
+export default function RecipeCard(props: {
+  recipeUrl: string
+  foodImageUrl: string
+  title: string
+  material: string
+}) {
+  return (
+    <div className={styles.root}>
+      <Link to={props.recipeUrl} target="_blank" rel="noopener noreferrer">
+        <img className={styles.iframe} src={props.foodImageUrl} />
+        <div className={styles.textbox}>
+          <div className={styles.text_title}>{props.title}</div>
+          <div className={styles.text_material}>{props.material}</div>
+          <TipReference url={props.recipeUrl} />
+        </div>
+      </Link>
+    </div>
+  )
+}
