24522f4ca1980fdd4393ecdaf0b1b04bd128c335
bvv-1 <fhinnoigsr@gmail.com>
2023-04-01 00:38:50 +0900

    文字の位置を調整

--- frontend/src/pages/HowTo.tsx
+++ frontend/src/pages/HowTo.tsx
@@ -19,7 +19,7 @@ export default function HowTo() {
     <>
       <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
         <SwiperSlide>
-          <div className="title_div">
+          <div className="text_div">
             <p className="description">質問に答えていくだけで献立を提案してくれるアプリ</p>
             <h1 className="title">だるめし</h1>
           </div>
@@ -37,9 +37,11 @@ export default function HowTo() {
         </SwiperSlide>
 
         <SwiperSlide>
-          <p className="heading">
-            まずは使いたい食材を<br></br>入力してみましょう
-          </p>
+          <div className="text_div">
+            <p className="heading">
+              まずは使いたい食材を<br></br>入力してみましょう
+            </p>
+          </div>
           <Smartphone img={img1} />
           <div className="bottom">
             <NextButton
@@ -54,9 +56,11 @@ export default function HowTo() {
         </SwiperSlide>
 
         <SwiperSlide>
-          <p className="heading">
-            いくつかの質問に<br></br>答えてみましょう
-          </p>
+          <div className="text_div">
+            <p className="heading">
+              いくつかの質問に<br></br>答えてみましょう
+            </p>
+          </div>
           <Smartphone img={img2} />
           <div className="bottom">
             <NextButton
@@ -71,9 +75,11 @@ export default function HowTo() {
         </SwiperSlide>
 
         <SwiperSlide>
-          <p className="heading">
-            あなたにぴったりの<br></br>レシピを検索可能
-          </p>
+          <div className="text_div">
+            <p className="heading">
+              あなたにぴったりの<br></br>レシピを検索可能
+            </p>
+          </div>
           <Smartphone img={img3} />
           <div className="bottom">
             <NextButton
