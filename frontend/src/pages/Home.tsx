c0834ff91511191ac73ed43c6586577ffc196783
Yokomi422 <rayzou1120@icloud.com>
2023-05-07 11:57:50 +0900

    HomeをFooterコンポーネントからCopyrightコンポーネントに変えた

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 import { useEffect } from "react"
 
-import Footer from "@/components/Footer/Footer"
+import Copyright from "../components/Copyright/Copyright"
 
 export default function Home() {
   // 永続的に残るので、localStorageから問題への回答を消しておく
@@ -28,7 +28,7 @@ export default function Home() {
           </Link>
         </div>
         <br></br>
-        <Footer />
+        <Copyright />
       </div>
     </>
   )
