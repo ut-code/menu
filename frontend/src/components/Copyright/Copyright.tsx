eddba39c452f8bb333fa686e14e35b84197736ac
Yokomi422 <rayzou1120@icloud.com>
2023-05-07 12:12:56 +0900

    /homeのcopyrightを下に持ってきた

--- frontend/src/components/Copyright/Copyright.tsx
+++ frontend/src/components/Copyright/Copyright.tsx
@@ -1,14 +1,18 @@
 import Link from "@mui/material/Link"
 import Typography from "@mui/material/Typography"
+import styles from "./Copyright.module.css"
+
 export default function Copyright() {
   return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://utcode.net/">
-        ut.code();
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
+    <div className={styles.copyright}>
+      <Typography variant="body2" color="text.secondary" align="center">
+        {"Copyright © "}
+        <Link color="inherit" href="https://utcode.net/">
+          ut.code();
+        </Link>{" "}
+        {new Date().getFullYear()}
+        {"."}
+      </Typography>
+    </div>
   )
 }
