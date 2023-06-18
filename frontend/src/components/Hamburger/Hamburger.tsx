19128890c990d24cafbbe6fc33a36cea78979437
bvv-1 <fhinnoigsr@gmail.com>
2023-06-18 12:11:32 +0900

    Create Hamburger component

---
+++ frontend/src/components/Hamburger/Hamburger.tsx
@@ -0,0 +1,11 @@
+import styles from "./Hamburger.module.css"
+
+export const Hamburger = () => {
+  return (
+    <>
+      <div className={styles.overlay}>
+        <div className={styles.content}>hello</div>
+      </div>
+    </>
+  )
+}
