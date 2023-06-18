87030494344adad7a9a0663b6a0d387a5c52c229
bvv-1 <fhinnoigsr@gmail.com>
2023-06-18 12:27:53 +0900

    Add links to hamburger menu

--- frontend/src/components/Hamburger/Hamburger.tsx
+++ frontend/src/components/Hamburger/Hamburger.tsx
@@ -1,10 +1,26 @@
+import { Link } from "react-router-dom"
 import styles from "./Hamburger.module.css"
 
 export const Hamburger = () => {
   return (
     <>
       <div className={styles.overlay}>
-        <div className={styles.content}>hello</div>
+        <div className={styles.content}>
+          <Link to={"/home"}>
+            <button>ホーム</button>
+          </Link>
+          <Link to={"/questions"}>
+            <button>検索する</button>
+          </Link>
+          <Link to={"/"}>
+            <button>このアプリの使い方</button>
+          </Link>
+          <Link to={"/home"}>
+            <button>設定</button>
+          </Link>
+          <button>English</button>
+          だるめし Dull Meshi
+        </div>
       </div>
     </>
   )
