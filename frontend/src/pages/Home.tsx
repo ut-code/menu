2a351b9531d2d5d837e31420855fe691ba062f89
bvv-1 <fhinnoigsr@gmail.com>
2023-06-18 12:12:08 +0900

    Show Humburger component in Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom"
 import { useEffect } from "react"
 
+import { Hamburger } from "@/components/Hamburger"
+
 export default function Home() {
   // 永続的に残るので、localStorageから問題への回答を消しておく
   useEffect(() => {
@@ -19,6 +21,7 @@ export default function Home() {
         <Link to={"/result"}>
           <button>検索結果</button>
         </Link>
+        <Hamburger />
       </div>
     </>
   )
