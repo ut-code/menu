13797a4a6d44a92965525d7a40c19804d9c2ccfc
bvv-1 <fhinnoigsr@gmail.com>
2023-06-18 15:23:34 +0900

    Add hamburger links and language button

--- frontend/src/components/Hamburger/Hamburger.tsx
+++ frontend/src/components/Hamburger/Hamburger.tsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom"
 import styles from "./Hamburger.module.css"
 
+// react-iconを入れる（仮置き）
+// react-iconsの置き換えは任せた！
+import { FaBars } from "react-icons/fa"
+
 interface Props {
   onClickClose: () => void
 }
@@ -9,17 +13,30 @@ export const Hamburger = (props: Props) => {
   return (
     <>
       <div className={styles.overlay}>
-        {/* react-iconsの置き換えは任せた！ */}
         <button onClick={props.onClickClose} className={styles.close}>
           x
         </button>
         <div className={styles.content}>
-          <Link to={"/home"}>ホーム</Link>
-          <Link to={"/questions"}>検索する</Link>
-          <Link to={"/"}>このアプリの使い方</Link>
-          <Link to={"/home"}>設定</Link>
-          <button>English</button>
-          ©だるめし Dull Meshi
+          <div className={styles.links}>
+            <Link to={"/home"} className={styles.link}>
+              <FaBars />
+              ホーム
+            </Link>
+            <Link to={"/questions"} className={styles.link}>
+              <FaBars />
+              検索する
+            </Link>
+            <Link to={"/"} className={styles.link}>
+              <FaBars />
+              このアプリの使い方
+            </Link>
+            <Link to={"/home"} className={styles.link}>
+              <FaBars />
+              設定
+            </Link>
+          </div>
+          <button className={styles.language}>English</button>
+          <div className={styles.footer}>©だるめし Dull Meshi</div>
         </div>
       </div>
     </>
