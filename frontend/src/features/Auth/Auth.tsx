6c274ac1cdcb81fe6bc830f1b890a0394d5e81c8
bvv-1 <fhinnoigsr@gmail.com>
2023-08-11 10:24:37 +0900

    fix types

--- frontend/src/features/Auth/Auth.tsx
+++ frontend/src/features/Auth/Auth.tsx
@@ -1,7 +1,7 @@
 import { Session } from "@supabase/supabase-js"
 import { SignIn } from "./SignIn"
 interface Props {
-  session: Session
+  session: Session | null
 }
 
 export const Auth = (props: Props) => {
--- frontend/src/features/Auth/SignIn/SignIn.tsx
+++ frontend/src/features/Auth/SignIn/SignIn.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react"
-import { Session } from "@supabase/supabase-js"
 import { supabase } from "../supabaseClient"
 
 export const SignIn = () => {
