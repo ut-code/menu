6c75ae6baeeb3543d94f9fd3a71d3f1f699c7bc7
bvv-1 <fhinnoigsr@gmail.com>
2023-08-12 00:49:05 +0900

    Create supabaseClient.test.ts

---
+++ frontend/src/features/Auth/supabaseClient.test.ts
@@ -0,0 +1,11 @@
+import { createClient } from "@supabase/supabase-js"
+
+const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey: string = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+describe("Supabase Client", () => {
+  it("should create a supabase client", () => {
+    const client = createClient(supabaseUrl, supabaseAnonKey)
+    expect(client).toBeDefined()
+  })
+})
