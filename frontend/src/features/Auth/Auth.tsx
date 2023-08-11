4959222e6862a0f2c706a0b9715e3bcba25f957f
bvv-1 <fhinnoigsr@gmail.com>
2023-08-11 12:48:02 +0900

    Enable to sign out

--- frontend/src/features/Auth/Auth.tsx
+++ frontend/src/features/Auth/Auth.tsx
@@ -1,9 +1,23 @@
 import { Session } from "@supabase/supabase-js"
+import { supabase } from "./supabaseClient"
 import { SignIn } from "./SignIn"
+
 interface Props {
   session: Session | null
 }
 
 export const Auth = (props: Props) => {
-  return !props.session ? <SignIn /> : <div>Already logged in</div>
+  async function signOut() {
+    const { error } = await supabase.auth.signOut()
+    if (error) console.log(error.message)
+  }
+
+  return !props.session ? (
+    <SignIn />
+  ) : (
+    <div>
+      <p>Already logged in</p>
+      <button onClick={signOut}>Sign out</button>
+    </div>
+  )
 }
