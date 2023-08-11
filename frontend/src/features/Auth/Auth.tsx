07e83ca5546ca9d985c0026a6f8d618d423cc991
bvv-1 <fhinnoigsr@gmail.com>
2023-08-11 22:34:22 +0900

    Update Auth.tsx

--- frontend/src/features/Auth/Auth.tsx
+++ frontend/src/features/Auth/Auth.tsx
@@ -1,23 +1,18 @@
 import { Session } from "@supabase/supabase-js"
-import { supabase } from "./supabaseClient"
 import { SignIn } from "./SignIn"
+import { SignOut } from "./SignOut"
 
 interface Props {
   session: Session | null
 }
 
 export const Auth = (props: Props) => {
-  async function signOut() {
-    const { error } = await supabase.auth.signOut()
-    if (error) console.log(error.message)
-  }
-
   return !props.session ? (
     <SignIn />
   ) : (
     <div>
       <p>Already logged in</p>
-      <button onClick={signOut}>Sign out</button>
+      <SignOut />
     </div>
   )
 }
