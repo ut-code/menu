0cacefdb8455a6b9563a2f5206a87dcd3a89cb17
bvv-1 <fhinnoigsr@gmail.com>
2023-09-01 23:50:40 +0900

    Add tanstack query

--- frontend/src/features/Favorite/Favorite.tsx
+++ frontend/src/features/Favorite/Favorite.tsx
@@ -1,59 +1,60 @@
-import { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 import { Session } from "@supabase/supabase-js"
 
 import { Recipe } from "@/utils/recipes"
 import { getUserFavoritesApi, deleteUserFavoritesApi } from "@/utils/apiUtils"
+import { useQueryClient, useQuery, useMutation } from "@tanstack/react-query"
 
 interface Props {
   session: Session | null
 }
 
 export const Favorite = ({ session }: Props) => {
-  const [favoriteRecipes, setFavoriteRecipes] = useState<Recipe[]>([])
-  const [loading, setLoading] = useState<boolean>(false)
+  const queryClient = useQueryClient()
 
-  useEffect(() => {
-    const fetchUserFavorites = async () => {
-      // NOTE: https://www.notion.so/utcode/JWT-4743f0e6a64e4ee7848818c9bc0efee1?pvs=4
-      if (!session) return
+  const {
+    data: favoriteRecipes,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["favoriteRecipes"],
+    queryFn: async () => {
+      if (!session) return []
       const response = await fetch(getUserFavoritesApi(), {
         headers: { Authorization: `Bearer ${session.access_token}` },
       })
       const recipes: Recipe[] = await response.json()
-      try {
-        setFavoriteRecipes(recipes)
-        console.log(recipes)
-      } catch (error) {
-        console.log(error)
-      }
-    }
+      return recipes
+    },
+  })
 
-    fetchUserFavorites()
-    setLoading(false)
-  }, [loading])
+  const onClickDeleteFavorite = useMutation({
+    mutationFn: async (recipeId: number) => {
+      if (!session) return
+      const response = await fetch(deleteUserFavoritesApi(recipeId), {
+        method: "DELETE",
+        headers: { Authorization: `Bearer ${session.access_token}` },
+      })
+      const userFavorite = await response.json()
+      console.log(userFavorite)
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["favoriteRecipes"])
+    },
+  })
 
-  const onClickDeleteFavorite = (recipeId: number) => async () => {
-    if (!session) return
-    const response = await fetch(deleteUserFavoritesApi(recipeId), {
-      method: "DELETE",
-      headers: { Authorization: `Bearer ${session.access_token}` },
-    })
-    const userFavorite = await response.json()
-    console.log(userFavorite)
-    setLoading(true)
-  }
-
+  if (isLoading) return <p>お気に入りを読み込み中</p>
+  if (isError) return <p>お気に入りの読み込みに失敗しました</p>
   return (
     <>
       <div className="style_lightbrown">
         <h1>お気に入り</h1>
-        {favoriteRecipes.length > 0 ? (
+        {favoriteRecipes ? (
           <ul>
             {favoriteRecipes.map((recipe) => (
               <span key={recipe.id}>
                 <li>{recipe.recipeTitle}</li>
-                <button onClick={onClickDeleteFavorite(recipe.id)}>お気に入りから削除</button>
+                <button onClick={() => onClickDeleteFavorite.mutate(recipe.id)}>お気に入りから削除</button>
               </span>
             ))}
           </ul>
