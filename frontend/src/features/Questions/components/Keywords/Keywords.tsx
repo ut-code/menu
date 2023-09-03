b0b42b88fd2d084d0375bc126c154bfc03078958
monmekoromo3 <136954680+monmekoromo3@users.noreply.github.com>
2023-09-03 12:45:33 +0900

    肉球を追加

--- frontend/src/features/Questions/components/Keywords/Keywords.tsx
+++ frontend/src/features/Questions/components/Keywords/Keywords.tsx
@@ -1,4 +1,5 @@
 import styles from "./Keywords.module.css"
+import { Footprint } from "@/components/elements/footprint"
 
 interface Props {
   answers: {
@@ -12,6 +13,7 @@ export const Keywords = ({ answers }: Props) => {
   return (
     <div className={styles.box}>
       <span className={styles.title}>あなたの検索ワード🔍&nbsp;&nbsp;</span>
+      <Footprint></Footprint>
       <br></br>
       <div className={styles.text}>{[...answers.ingredients, answers.genre, answers.cookingTime].join(" ")}</div>
     </div>
