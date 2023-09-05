2f2899d4a97fd1c37fc54345de38ad5690605123
bvv-1 <fhinnoigsr@gmail.com>
2023-09-05 19:45:12 +0900

    Fix Card css

--- frontend/src/features/Home/components/Card/Card.tsx
+++ frontend/src/features/Home/components/Card/Card.tsx
@@ -13,18 +13,15 @@ export const Card = ({ recipe }: Props) => {
   return (
     <Link to={recipe.recipeUrl} target="_blank" rel="noopener noreferrer">
       <div className={styles.root}>
-        <div className={styles.image_box}>
-          {/* <img className={styles.image_frame_blur} src={recipe.foodImageUrls[0]} /> */}
-          <img className={styles.image_frame} src={recipe.foodImageUrls[0]} />
-        </div>
-        <div className={styles.hr}>{"-"}</div>
+        <img className={styles.image_frame} src={recipe.foodImageUrls[0]} />
+        <div className={styles.hr}>{"_"}</div>
         <div className={styles.text_container}>
           <div className={styles.left}>
             <span className={styles.text_title}>{recipe.recipeTitle}</span>
             <span className={styles.text_material}>{recipeMaterialConverted}</span>
           </div>
-          <div className={styles.arrow}>
-            <BsArrowRight size="1rem" color="white" />
+          <div className={styles.icon}>
+            <BsArrowRight color="white" />
           </div>
         </div>
       </div>
