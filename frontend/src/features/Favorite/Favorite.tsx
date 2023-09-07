82e220a1d02b3de7dca39c8e402742618645c6fd
bvv-1 <fhinnoigsr@gmail.com>
2023-09-07 23:00:17 +0900

    fix: visuals

--- frontend/src/features/Favorite/Favorite.tsx
+++ frontend/src/features/Favorite/Favorite.tsx
@@ -1,9 +1,14 @@
-import { Link } from "react-router-dom"
+import { useState, useEffect } from "react"
+import { Link, useNavigate } from "react-router-dom"
 import { Session } from "@supabase/supabase-js"
 import { useQueryClient, useQuery, useMutation } from "@tanstack/react-query"
 
+import { getUserFavoritesApi, postUserFavoritesApi, deleteUserFavoritesApi } from "@/utils/apiUtils"
 import { Recipe } from "@/utils/recipes"
-import { getUserFavoritesApi, deleteUserFavoritesApi } from "@/utils/apiUtils"
+import { Head } from "@/components/Head"
+import { RecipeCard } from "@/components/RecipeCard"
+import { Hamburger } from "@/components/Hamburger"
+import styles from "./Favorite.module.css"
 
 interface Props {
   session: Session | null
@@ -11,6 +16,9 @@ interface Props {
 
 export const Favorite = ({ session }: Props) => {
   const queryClient = useQueryClient()
+  const Navigate = useNavigate()
+
+  const [isOpenHamburger, setIsOpenHamburger] = useState<boolean>(false)
 
   // NOTE: https://www.notion.so/utcode/JWT-4743f0e6a64e4ee7848818c9bc0efee1?pvs=4
   const { data: favoriteRecipes, isLoading } = useQuery({
@@ -23,6 +31,24 @@ export const Favorite = ({ session }: Props) => {
       if (!response.ok) throw new Error("お気に入りの取得に失敗しました")
       const recipes: Recipe[] = await response.json()
       return recipes
+    },
+  })
+
+  // NOTE: https://www.notion.so/utcode/JWT-4743f0e6a64e4ee7848818c9bc0efee1?pvs=4
+  const onClickAddFavorite = useMutation({
+    mutationFn: async (recipeId: number) => {
+      if (!session?.access_token) return []
+      const response = await fetch(postUserFavoritesApi(), {
+        method: "POST",
+        headers: { "Content-Type": "application/json", Authorization: `Bearer ${session?.access_token}` },
+        body: JSON.stringify({ recipeId: recipeId }),
+      })
+      if (!response.ok) throw new Error("お気に入りの追加に失敗しました")
+      const userFavorite = await response.json()
+      console.log(userFavorite)
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["favoriteRecipes"])
     },
   })
 
@@ -43,28 +69,49 @@ export const Favorite = ({ session }: Props) => {
     },
   })
 
+  const toggleFavorite = (recipeId: number) => {
+    if (!favoriteRecipes) return
+    if (favoriteRecipes.some((recipe) => recipe.id === recipeId)) {
+      onClickDeleteFavorite.mutate(recipeId)
+    } else {
+      onClickAddFavorite.mutate(recipeId)
+    }
+  }
+
+  const onClickOpenHamburger = () => setIsOpenHamburger(true)
+  const onClickCloseHamburger = () => setIsOpenHamburger(false)
+
   if (isLoading) return <p>お気に入りを読み込み中</p>
   return (
-    <>
-      <div className="style_lightbrown">
-        <h1>お気に入り</h1>
+    <div className="style_lightbrown">
+      <Head
+        showBackButton={true}
+        onClickPreviousPage={() => Navigate("/home")}
+        onClickOpenHamburger={onClickOpenHamburger}
+      />
+      {isOpenHamburger === true && <Hamburger onClickCloseHamburger={onClickCloseHamburger} />}
+
+      <h1>お気に入り</h1>
+      <div className={styles.cards}>
         {favoriteRecipes ? (
-          <ul>
-            {favoriteRecipes.map((recipe) => (
-              <span key={recipe.id}>
-                <li>{recipe.recipeTitle}</li>
-                <button onClick={() => onClickDeleteFavorite.mutate(recipe.id)}>お気に入りから削除</button>
-              </span>
-            ))}
-          </ul>
+          favoriteRecipes.map((recipe) => (
+            <RecipeCard
+              key={recipe.id}
+              recipe={recipe}
+              favoriteRecipes={favoriteRecipes}
+              toggleFavorite={toggleFavorite}
+              session={session}
+            />
+          ))
         ) : (
           // TODO: サインインしてないときに表示を変える
           <p>お気に入りはまだありません。ハートボタンを押して追加してみましょう。</p>
         )}
-        <Link to="/home">
-          <button>戻る</button>
-        </Link>
       </div>
-    </>
+
+      <Link to="/home">
+        <button>戻る</button>
+      </Link>
+    </div>
   )
 }
