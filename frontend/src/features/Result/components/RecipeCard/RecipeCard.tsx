3204fc373bf80bc6a9561d9ceebb8bfba30fb3ac
bvv-1 <fhinnoigsr@gmail.com>
2023-09-07 22:20:53 +0900

    Add e.preventDefault

--- frontend/src/features/Result/components/RecipeCard/RecipeCard.tsx
+++ frontend/src/features/Result/components/RecipeCard/RecipeCard.tsx
@@ -8,19 +8,34 @@ import styles from "./RecipeCard.module.css"
 
 interface Props {
   recipe: Recipe
+  favoriteRecipes: Recipe[] | undefined
+  toggleFavorite: (recipeId: number) => void
   session: Session | null
 }
 
-export const RecipeCard = ({ recipe, session }: Props) => {
+export const RecipeCard = ({ recipe, favoriteRecipes, toggleFavorite, session }: Props) => {
   const recipeMaterialConverted = recipe.recipeMaterial.join("・")
+
+  const isFavorite: boolean =
+    favoriteRecipes !== undefined && favoriteRecipes.some((favoriteRecipe) => favoriteRecipe.id === recipe.id)
+
+  const onClickHandler = (recipeId: number, event: React.MouseEvent<HTMLDivElement>) => {
+    if (!session) return
+    // NOTE: LinkとonClickは別メソッド？なので event.stopPropagation() だとうまく行かなかった
+    event.preventDefault()
+    toggleFavorite(recipeId)
+  }
 
   return (
     <Link to={recipe.recipeUrl} target="_blank" rel="noopener noreferrer">
       <div className={styles.root}>
         <img className={styles.image_frame} src={recipe.foodImageUrls[0]} />
         {session && (
-          <div className={styles.icon}>
-            <AiFillHeart color="red" />
+          <div
+            className={styles.icon}
+            onClick={(event: React.MouseEvent<HTMLDivElement>) => onClickHandler(recipe.id, event)}
+          >
+            <AiFillHeart color={isFavorite ? "red" : "gray"} />
           </div>
         )}
         <div className={styles.hr}>{"_"}</div>
