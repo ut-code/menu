8cf3aea1c6ccec9ffeab917f992ffa2037bfe609
双葉 <futabamusic20@gmail.com>
2023-09-23 16:53:04 +0900

    不要なコードを消しました

--- frontend/src/features/Questions/components/QuestionIngredients/QuestionIngredients.tsx
+++ frontend/src/features/Questions/components/QuestionIngredients/QuestionIngredients.tsx
@@ -4,9 +4,6 @@ import { Session } from "@supabase/supabase-js"
 
 import { NextButton } from "@/components/elements/button/NextButton"
 import { Searchbox } from "@/components/Searchbox"
-import { Option, allIngredients, shuffleOptions } from "@/utils/questions"
-import iconPlus from "@/assets/icon/icon_plus.svg"
-import { BsCheckLg } from "react-icons/bs"
 import { Head } from "@/components/Head"
 import { Hamburger } from "@/components/Hamburger"
 
@@ -17,28 +14,15 @@ interface Props {
   session: Session | null
 }
 
-const shuffledOptions: Option[] = shuffleOptions(allIngredients)
-
 export const QuestionIngredients = ({ setQuestionNumber, ingredients, setIngredients, session }: Props) => {
   const Navigate = useNavigate()
   const [inputContent, setInputContent] = useState<string>("")
-  const [showIngredientsNumber, setShowIngredientsNumber] = useState<number>(3)
   const [isOpenHamburger, setIsOpenHamburger] = useState<boolean>(false)
   useEffect(() => {
     if (ingredients !== undefined) {
       setInputContent(ingredients.join(" "))
     }
   }, [ingredients])
-  const options: Option[] = shuffledOptions.slice(0, showIngredientsNumber)
-
-  const toggleOption = (value: string) => {
-    if (!ingredients) return
-    if (ingredients.includes(value)) {
-      setIngredients(ingredients.filter((ingredient) => ingredient !== value))
-    } else {
-      setIngredients([...ingredients, value])
-    }
-  }
 
   // NOTE: inputContentを変化させたらingredientsも変化させている
   const onChangeSearchbox = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -46,21 +30,9 @@ export const QuestionIngredients = ({ setQuestionNumber, ingredients, setIngredi
     setIngredients(e.target.value.split(" "))
   }
 
-  const onChangeCheckbox = (e: React.ChangeEvent<HTMLInputElement>) => {
-    toggleOption(e.target.value)
-    if (ingredients !== undefined) {
-      setInputContent(ingredients.join(" "))
-    }
-  }
-
-  const incrementIngredientsNumber = () => {
-    setShowIngredientsNumber((prev) => (prev + 1 < shuffledOptions.length ? prev + 1 : prev))
-  }
-
   const onClickNextPage = () => {
     if (ingredients === undefined || ingredients.length === 0) {
-      // FIXME: アラートを実装する
-      alert("選択肢を選んでください")
+      alert("食材を入力してください")
       return
     }
     setQuestionNumber(1)
@@ -93,8 +65,7 @@ export const QuestionIngredients = ({ setQuestionNumber, ingredients, setIngredi
         />
         <div style={{ paddingLeft: 16, paddingRight: 16, marginTop: 24, marginBottom: 96 }}>
           <h3 style={{ marginBottom: 8 }}>入力のヒント💡</h3>
-          <p style={{ marginBottom: 4 }}>食材は2個以上入力することができます(例：玉ねぎ 牛乳)</p>
-          <p>食材を2個以上入力するときは、スペースを空けてください</p>
+          <p style={{ marginBottom: 4 }}>食材を2個以上入力するときは、スペースを空けてください(例：玉ねぎ 牛乳)</p>
         </div>
 
         <NextButton onClick={onClickNextPage} />
