209f2efbbc675541da856119b00e766d33da4adc
SatohiroToko <142480159+SatohiroToko@users.noreply.github.com>
2023-09-23 15:13:03 +0900

    Added text to the loading page

--- frontend/src/components/Loading/Loading.tsx
+++ frontend/src/components/Loading/Loading.tsx
@@ -1,5 +1,10 @@
 import styles from "./Loading.module.css"
 
 export const Loading = () => {
-  return <div className={styles.loader}></div>
+  return (
+    <div className={styles.centerContainer}>
+      <div className={styles.loader}></div>
+      <h3 className={styles.loadingText}>読み込み中</h3>
+    </div>
+  )
 }
