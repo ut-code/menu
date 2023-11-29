69f0bc23b9628ce7f7962becbe50b1ebc938efae
bvv-1 <fhinnoigsr@gmail.com>
2023-11-29 22:48:27 +0900

    fix: image name

--- frontend/src/components/EmptyResults/EmptyResults.tsx
+++ frontend/src/components/EmptyResults/EmptyResults.tsx
@@ -1,12 +1,12 @@
 //import { SwiperSlide } from "swiper/react"
 import styles from "./EmptyResults.module.css"
 //import { EmptyResults } from "@/components/EmptyResults"
-import Sorry from "@/assets/image/Sorry.png"
+import imgSorry from "@/assets/image/Sorry.png"
 
 export const EmptyResults = () => {
   return (
     <div className={styles.centerContainer}>
-      <img src={Sorry} alt="アイコン" className={styles.icon} />
+      <img src={imgSorry} alt="イラスト" className={styles.icon} />
       <div className={styles.combinedText}>
         <h2>検索結果が見つかりません</h2>
         <div className={styles.smallExplanationText}>
