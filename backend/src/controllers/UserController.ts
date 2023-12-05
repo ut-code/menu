717584d7175ab1b1d72294f1f45400eacc85f8c9
bvv-1 <fhinnoigsr@gmail.com>
2023-12-05 20:42:14 +0900

    fix: use arrow function for member method

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -4,8 +4,10 @@ import { supabase } from "../supabaseClient"
 import { User } from "@supabase/supabase-js"
 import type { Users } from "@prisma/client"
 
+// NOTE: https://github.com/Microsoft/TypeScript/wiki/'this'-in-TypeScript#use-instance-functions
+// メンバ関数にはアロー関数を使っておくと this が undefined にならずに済む
 class UserController {
-  async getUser(req: Request, res: Response): Promise<void> {
+  getUser = async (req: Request, res: Response): Promise<void> => {
     const userFromRequest = await this.extractUserFromRequest(req)
     if (!userFromRequest) {
       res.status(401).json({ error: "Not authorized" })
@@ -21,7 +23,7 @@ class UserController {
     res.status(200).json(user)
   }
 
-  private async getUserById({ userId }: { userId: Users["id"] }) {
+  private getUserById = async ({ userId }: { userId: Users["id"] }) => {
     return client.users.findUnique({
       where: {
         id: userId,
@@ -30,7 +32,7 @@ class UserController {
   }
 
   // NOTE: https://www.notion.so/utcode/JWT-4743f0e6a64e4ee7848818c9bc0efee1?pvs=4
-  private async extractUserFromRequest(req: Request): Promise<User | null> {
+  private extractUserFromRequest = async (req: Request): Promise<User | null> => {
     const authorizationHeader = req.headers["authorization"]
     if (!authorizationHeader) {
       return null
@@ -42,7 +44,10 @@ class UserController {
     const token = tokenMatch[1]
 
     const user = await supabase.auth.getUser(token)
-    return user?.data?.user
+    if (!user) {
+      return null
+    }
+    return user.data.user
   }
 }
 
