e7f6cfe046b074e768aa54255eb0cc54c6bc3658
bvv-1 <fhinnoigsr@gmail.com>
2023-12-06 09:43:18 +0900

    refactor: made searchRecipes more readable

--- backend/src/controllers/SearchController.ts
+++ backend/src/controllers/SearchController.ts
@@ -9,25 +9,10 @@ class SearchController {
     try {
       const { searchInfo } = req.body
 
-      const ingredientsAndQuery: string = searchInfo.ingredients.join(" & ")
-      const dishQuery = searchInfo.dish
-      let cookingTimeQuery = {}
-      switch (searchInfo.cookingTime) {
-        case "時短":
-          cookingTimeQuery = { gte: 1, lt: 20 }
-          break
-        case "普通":
-          cookingTimeQuery = { gte: 20, lt: 45 }
-          break
-        case "じっくり":
-          cookingTimeQuery = { gte: 45 }
-          break
-        default:
-          break
-      }
+      const ingredientsAndQuery = this.joinIngredients(searchInfo.ingredients)
+      const cookingTimeQuery = this.getCookingTimeQuery(searchInfo.cookingTime)
 
-      const orderBy = Math.random() < 0.5 ? "description" : "materialsConverted"
-      const orderDir = Math.random() < 0.5 ? "asc" : "desc"
+      const { orderBy, orderDir } = this.getRandomOrder()
       // const itemCount = await client.recipes.count()
       // const skip = Math.max(0, Math.floor(Math.random() * itemCount) - 5000)
 
@@ -41,18 +26,40 @@ class SearchController {
           materialsConverted: {
             search: ingredientsAndQuery,
           },
-          dish: dishQuery,
+          dish: searchInfo.dish,
           totalCookingTime: cookingTimeQuery,
         },
         take: 20,
       })
-      console.log(searchInfo)
-      console.log(recipes)
+
       res.json(recipes)
     } catch (error) {
       console.error(error)
       res.status(500).json({ error: "Internal server error" })
     }
+  }
+
+  private joinIngredients = (ingredients: string[]): string => {
+    return ingredients.join(" & ")
+  }
+
+  private getCookingTimeQuery = (cookingTime: string): Record<string, number> => {
+    switch (cookingTime) {
+      case "時短":
+        return { gte: 1, lt: 20 }
+      case "普通":
+        return { gte: 20, lt: 45 }
+      case "じっくり":
+        return { gte: 45 }
+      default:
+        return {}
+    }
+  }
+
+  private getRandomOrder = (): { orderBy: string; orderDir: string } => {
+    const orderBy = Math.random() < 0.5 ? "description" : "materialsConverted"
+    const orderDir = Math.random() < 0.5 ? "asc" : "desc"
+    return { orderBy, orderDir }
   }
 }
 
