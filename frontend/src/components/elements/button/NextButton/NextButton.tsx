aa825aa10089ebd28c21239c3796c78711e2c6fa
双葉 <futabamusic20@gmail.com>
2024-03-24 12:08:51 +0900

    NextButtonのスタイルを修正

--- frontend/src/components/elements/button/NextButton/NextButton.tsx
+++ frontend/src/components/elements/button/NextButton/NextButton.tsx
@@ -3,12 +3,13 @@ import styles from "./NextButton.module.css"
 interface Props {
   onClick?: () => void
   title?: string
+  disabled?: boolean
 }
 
-export const NextButton = ({ onClick, title }: Props) => {
+export const NextButton = ({ onClick, title, disabled }: Props) => {
   return (
-    <div className={styles.root}>
-      <button className={styles.button} onClick={onClick}>
+    <div>
+      <button className={styles.root} onClick={onClick} disabled={disabled}>
         {title}
       </button>
     </div>
