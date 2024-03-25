e01a8dbedcb7c99fa03b20aa81b59b477d718b02
双葉 <futabamusic20@gmail.com>
2024-03-25 13:46:14 +0900

    ボタンと認証画面のスタイルを変更

--- frontend/src/components/elements/button/BorderButton/BorderButton.tsx
+++ frontend/src/components/elements/button/BorderButton/BorderButton.tsx
@@ -3,18 +3,18 @@ import styles from "./BorderButton.module.css"
 interface Props {
   onClick: () => void
   disabled: boolean
-  isWhite?: boolean
+  isBlack?: boolean
   children: React.ReactNode
 }
 
-export const BorderButton = ({ onClick, disabled, isWhite, children }: Props) => {
+export const BorderButton = ({ onClick, disabled, isBlack, children }: Props) => {
   const buttonStyle = {
-    backgroundColor: isWhite ? "white" : "#f5b72e",
+    borderColor: isBlack ? "#000" : "#c4c4c4",
   }
 
   return (
     <button className={styles.button} onClick={onClick} disabled={disabled} style={buttonStyle}>
-      <h3>{children}</h3>
+      <h4>{children}</h4>
     </button>
   )
 }
