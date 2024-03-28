8b91869ccd205246145b7d0b1d6df8f024ebebbd
bvv-1 <fhinnoigsr@gmail.com>
2024-03-29 01:13:42 +0900

    Update BottomNavigationBar.tsx

--- frontend/src/components/BottomNavigationBar/BottomNavigationBar.tsx
+++ frontend/src/components/BottomNavigationBar/BottomNavigationBar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useLocation } from "react-router-dom"
 import BottomNavigation from "@mui/material/BottomNavigation"
 import BottomNavigationAction from "@mui/material/BottomNavigationAction"
 import Paper from "@mui/material/Paper"
@@ -12,33 +12,32 @@ import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined
 import AccountCircleIcon from "@mui/icons-material/AccountCircle"
 
 export const BottomNavigationBar = () => {
-  const [value, setValue] = useState(0)
-  const handleChange = (event: React.SyntheticEvent<Element, Event>, newValue: number) => {
-    setValue(newValue)
-  }
   const navigate = useNavigate()
+  const location = useLocation()
+  const [value, setValue] = useState(location.pathname)
+  const handleChange = (event: React.SyntheticEvent<Element, Event>, newValue: string) => {
+    setValue(newValue)
+    navigate(newValue)
+  }
 
   return (
-    <Paper
-      sx={{ "& .MuiBottomNavigationAction-root": { color: "black" }, position: "fixed", bottom: 0, left: 0, right: 0 }}
-      elevation={3}
-    >
+    <Paper sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }} elevation={3}>
       <BottomNavigation showLabels={true} value={value} onChange={handleChange}>
-        <BottomNavigationAction label="検索" icon={<SearchIcon />} onClick={() => navigate("/questions")} />
+        <BottomNavigationAction label="検索" icon={<SearchIcon />} value="/questions" />
         <BottomNavigationAction
           label="作成"
-          icon={value === 1 ? <EditIcon /> : <EditOutlinedIcon />}
-          onClick={() => navigate("/new")}
+          icon={value === "/new" ? <EditIcon /> : <EditOutlinedIcon />}
+          value="/new"
         />
         <BottomNavigationAction
           label="お気に入り"
-          icon={value === 2 ? <FavoriteIcon /> : <FavoriteBorderOutlinedIcon />}
-          onClick={() => navigate("/favorites")}
+          icon={value === "/favorites" ? <FavoriteIcon /> : <FavoriteBorderOutlinedIcon />}
+          value="/favorites"
         />
         <BottomNavigationAction
           label="マイページ"
-          icon={value === 3 ? <AccountCircleIcon /> : <AccountCircleOutlinedIcon />}
-          onClick={() => navigate("/setting")}
+          icon={value === "/setting" ? <AccountCircleIcon /> : <AccountCircleOutlinedIcon />}
+          value="/setting"
         />
       </BottomNavigation>
     </Paper>
