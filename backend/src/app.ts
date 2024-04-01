c5a643db4684fb438e72c4db52058b7f31404868
bvv-1 <fhinnoigsr@gmail.com>
2024-04-01 18:39:10 +0900

    Fix health check

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,7 +3,7 @@ import cors from "cors"
 import UserController from "./controllers/UserController"
 import SearchController from "./controllers/SearchController"
 import RecipeController from "./controllers/RecipeController"
-import { elasticSearchClient } from "./elasticSearchClient"
+import { serverUrl } from "./elasticSearchClient"
 
 const app = express()
 
@@ -20,16 +20,15 @@ app.get("/", (req, res) => {
   res.send("Hello, world!")
 })
 
-app.get("/api/elasticsearch/health", (req, res) => {
+app.get("/api/elasticsearch/health", async (req, res) => {
   try {
-    const health = elasticSearchClient.getServiceVersion()
-    res.json({
-      status: "ok",
-      health: health,
-    })
+    const health = await fetch(`${serverUrl}`)
+    if (health.ok) {
+      res.status(200).json({ status: "ok" })
+    }
   } catch (error) {
     console.error(error)
-    res.status(500).json({ error: "Elasticsearch is not available" })
+    res.status(500).json({ error: "Elasticsearch is down" })
   }
 })
 
