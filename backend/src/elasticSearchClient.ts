e1a331f3e6f9ed1ad21c5098056b99641cde0bc0
bvv-1 <fhinnoigsr@gmail.com>
2024-04-01 18:00:55 +0900

    Create elasticSearchClient.ts

---
+++ backend/src/elasticSearchClient.ts
@@ -0,0 +1,9 @@
+import apm from "elastic-apm-node"
+
+const serviceName = process.env.ELASTIC_APM_SERVICE_NAME ?? ""
+const serverUrl = process.env.ELASTIC_APM_SERVER_URL ?? ""
+
+export const elasticSearchClient = apm.start({
+  serviceName: serviceName,
+  serverUrl: serverUrl,
+})
