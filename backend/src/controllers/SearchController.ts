41af617f5d5b9feff2b7c8282d1dc1ad93082630
bvv-1 <fhinnoigsr@gmail.com>
2024-05-11 00:25:08 +0900

    fix boost

--- backend/src/controllers/SearchController.ts
+++ backend/src/controllers/SearchController.ts
@@ -66,10 +66,10 @@ class SearchController {
                     positive: {
                       bool: {
                         should: [
-                          { term: { title: { value: materials.length > 0 ? materials[0] : "", boost: 2 } } },
-                          { term: { description: { value: materials.length > 0 ? materials[0] : "", boost: 2 } } },
-                          { terms: { materials: materials, boost: 1.5 } },
-                          { term: { dish: { value: dish, boost: 2 } } },
+                          { term: { title: { value: materials.length > 0 ? materials[0] : "", boost: 1.5 } } },
+                          { term: { description: { value: materials.length > 0 ? materials[0] : "", boost: 1 } } },
+                          { terms: { materials: materials, boost: 1 } },
+                          { term: { dish: { value: dish, boost: 1 } } },
                           cookingTimeQuery,
                         ],
                       },
